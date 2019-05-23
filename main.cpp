c656ab0319fcfd11142dc85cdd74d93d672f1f25
Sergej Koehn <koehn@tetronik.com>
2019-05-23 17:52:19 +0200

    Main files and project.

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+ * SIDsynth.cpp
+ *
+ * Created: 23.05.2019 15:40:39
+ * Author : koehn
+ */ 
+
+#include <avr/io.h>
+
+#include "SID/SID.h"
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
